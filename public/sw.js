4e404dd63d3559733fbc3d9e0ad5dd8e15acfa70
fajrinabdillah <fajrinabdillah@gmail.com>
2026-06-06 12:15:50 +0700

    Delete public/sw.js

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (e) => {
-  // Wajib ada event fetch kosong ini agar syarat instalasi browser terpenuhi
-});
